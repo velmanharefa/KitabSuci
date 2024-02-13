f83710112ea42bf1243af2838405f14113d1ed59
velmanharefa <velmanh24@gmail.com>
2024-02-13 13:11:08 +0700

    abrakadabra

--- kitab_suci/lib/screen/loginPage.dart
+++ kitab_suci/lib/screen/loginPage.dart
@@ -10,6 +10,9 @@ class loginPage extends StatefulWidget {
 }
 
 class _loginPageState extends State<loginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   void _back() {
     Navigator.push(
       context, MaterialPageRoute(builder: (context) => const introPage3()));
@@ -77,42 +80,98 @@ class _loginPageState extends State<loginPage> {
               ],
             )
           ),
-
-          SizedBox(height: 300,),
           
+          SizedBox(height: 50),
+
           Container(
-            width: 271,
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Enter email",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Color(0xFFABABAB),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 271,
-                  // decoration: BoxDecoration(
-                  //   border: Border.all(
-                  //     color: Colors.black,
-                  //     width: 2,
-                  //   )
-                  // ),
-                  child: Divider(
-                    height: 20,
-                    color: Colors.black,
-                    thickness: 1,
-                  ),
-                )
-              ]
-              ),
-          )
+            child: Image.asset(
+              'assets/images/loginpic.png',
+            ),
+          ),
+
+          SizedBox(height: 50),
+          
+          // Container(
+          //   width: 300,
+          //   decoration: BoxDecoration(
+          //     border: Border.all(
+          //       color: Colors.black, // Warna outline
+          //       width: 2, // Ketebalan outline
+          //     ),
+          //   ),
+          //   child: Column(
+              
+          //     crossAxisAlignment:CrossAxisAlignment.start,
+          //     children: [
+          //       _buildGreyText("Enter email"),
+          //       const SizedBox(height: 10,),
+          //       _buildGreyText("Enter password"),
+          //       _buildInputField(emailController),
+          //     ]
+          //     ),
+          // ),
+          
+
+          // Container(
+          //   width: 271,
+          //   child: Column(
+          //     children: [
+          //       Container(
+          //         alignment: Alignment.centerLeft,
+          //         child: Text(
+          //           "Enter email",
+          //           style: TextStyle(
+          //             fontSize: 14,
+          //             color: Color(0xFFABABAB),
+          //           ),
+          //         ),
+          //       ),
+          //       Container(
+          //         width: 271,
+          //         // decoration: BoxDecoration(
+          //         //   border: Border.all(
+          //         //     color: Colors.black,
+          //         //     width: 2,
+          //         //   )
+          //         // ),
+          //         child: Divider(
+          //           height: 20,
+          //           color: Colors.black,
+          //           thickness: 1,
+          //         ),
+          //       )
+          //     ]
+          //     ),
+          // )
         ],
       ),
     );
   }
+
+  Widget _buildForm(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Welcome",
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildGreyText(String text){
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.grey),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller, {isPassword = false}) {
+    return TextField();
+  }
+
 }
+
