04c1e0d22d2e8149ccf157cd4c8f49c20186671c
BenayaObed <91049664+BenayaObed@users.noreply.github.com>
2024-02-15 22:00:41 +0700

    homepage

--- kitab_suci/lib/screen/accountPage.dart
+++ kitab_suci/lib/screen/accountPage.dart
@@ -31,25 +31,34 @@ class BluePainter extends CustomPainter {
 
     Path mainBackground = Path();
     mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
-    paint.color = const Color.fromARGB(255, 255, 255, 255);
+    paint.color = Colors.black;
     canvas.drawPath(mainBackground, paint);
 
   Path ovalPath = Path();
-    // Start paint from 20% height to the left
-    ovalPath.moveTo(height, width);
+    //Start paint from 20% height to the left
+    ovalPath.moveTo(0, 0);
 
     //paint a curve from current position to middle of the screen
     ovalPath.quadraticBezierTo(
-        width * 0.5, height * 0.2, width * 0.1, height * 0.5);
+        0, height*0.1, 0,height*0.2);
+    ovalPath.quadraticBezierTo(
+        width, height*0.3, width,0);
 
-    // Paint a curve from current position to bottom left of screen at width * 0.1
-    ovalPath.quadraticBezierTo(width * 0.1, height * 0.8, width * 0.5, height);
+    // ovalPath.conicTo(
+    //   size.width / 1, 1 * size.height / 2, size.width, 0, 20
+    // );
 
-    // // draw remaining line to bottom left side
-    // ovalPath.lineTo(0, height);
+
+    // ovalPath.relativeCubicTo(size.width / 4, 3 * size.height / 4, 3 * size.width / 4, size.height / 4, size.width, 0);
+
+    // // Paint a curve from current position to bottom left of screen at width * 0.1
+    // ovalPath.quadraticBezierTo(width * 0.1, height * 0.8, width * 0.5, height);
+
+    // draw remaining line to bottom left side
+    ovalPath.lineTo(width,0);
 
     // // Close line to reset it back
-    // ovalPath.close();
+    ovalPath.close();
 
     paint.color = Color(0xFF4DACB2);
     canvas.drawPath(ovalPath, paint);
--- kitab_suci/lib/screen/homePage.dart
+++ kitab_suci/lib/screen/homePage.dart
@@ -18,7 +18,8 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: CustomPaint(
+      painter:BluePainter(),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical:  10, horizontal: 20),
         child: Column(
@@ -362,5 +363,41 @@ class _homePageState extends State<homePage> {
         ),
       ]),
     );
+  }
+}
+
+class BluePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height;
+    final width = size.width;
+    Paint paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    paint.color = Colors.white;
+    canvas.drawPath(mainBackground, paint);
+
+  Path ovalPath = Path();
+    //Start paint from 20% height to the left
+    ovalPath.moveTo(0, 0);
+
+    //paint a curve from current position to middle of the screen
+    ovalPath.quadraticBezierTo(
+        0, height*0.2, 0,height*0.15);
+    ovalPath.quadraticBezierTo(
+        width, height*0.2, width,height*0.15);
+
+    ovalPath.lineTo(width,0);
+
+    ovalPath.close();
+
+    paint.color = Color(0xFF64FCD9);
+    canvas.drawPath(ovalPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
   }
 }
