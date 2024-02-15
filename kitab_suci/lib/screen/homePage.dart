24d3b656f9a6fc3a7735d7a2fa70f900f6d7b718
velmanharefa <velmanh24@gmail.com>
2024-02-15 20:38:08 +0700

    homepage

--- kitab_suci/lib/screen/homePage.dart
+++ kitab_suci/lib/screen/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:kitab_suci/screen/loginPage.dart';
 
 class homePage extends StatefulWidget {
   const homePage({super.key});
@@ -18,17 +19,251 @@ class _homePageState extends State<homePage> {
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 50),
+            SizedBox(height: 120),
             
-            TextFormField(
-              decoration: InputDecoration(
-                // https://www.youtube.com/watch?v=yumtaMvUnC8&ab_channel=TheTechBrothers
-                hintText: 'Search bible, verses...',
-                border: const OutlineInputBorder()
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
               ),
-              
-              
+              child: TextFormField(
+                
+                decoration: InputDecoration(
+                  // https://www.youtube.com/watch?v=yumtaMvUnC8&ab_channel=TheTechBrothers
+                  hintText: 'Search bible, verses...',
+                  border: InputBorder.none,
+                  // border: const OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(14),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
             ),
+
+            SizedBox(height: 5),
+
+            Container(
+              padding: EdgeInsets.all(7.0),
+              // decoration: BoxDecoration(
+              //   border: Border.all(
+              //     color: Colors.black,
+              //     width: 2,
+              //   )
+              // ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                
+                children: [
+                  InkWell(
+                    child: const Text(
+                      'Recent Reads',
+                      style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // onTap: (){
+                    //   Navigator.push(
+                    //     context, 
+                    //     MaterialPageRoute(
+                    //       builder: (context) => const loginPage()));
+                    // },
+                  ),
+
+                  InkWell(
+                    child: const Text(
+                      'See All',
+                      style: TextStyle(
+                        color: Color(0xFF4DACB2),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // onTap: (){
+                    //   Navigator.push(
+                    //     context, 
+                    //     MaterialPageRoute(
+                    //       builder: (context) => const loginPage()));
+                    // },
+                  ),
+                ],
+              ),
+            ),
+            
+            SizedBox(height: 5),
+
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border : Border.all(
+                  color: Colors.black,
+                  width: 1,
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  InkWell(
+                    child: const Text(
+                      'Sorry you need login',
+                      style: TextStyle(
+                        color: Color(0xFF4DACB2),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onTap: (){
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(
+                          builder: (context) => const loginPage()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 5),
+
+            Container(
+              padding: EdgeInsets.all(7.0),
+              // decoration: BoxDecoration(
+              //   border: Border.all(
+              //     color: Colors.black,
+              //     width: 2,
+              //   )
+              // ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                
+                children: [
+                  InkWell(
+                    child: const Text(
+                      'Explore Scripture',
+                      style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // onTap: (){
+                    //   Navigator.push(
+                    //     context, 
+                    //     MaterialPageRoute(
+                    //       builder: (context) => const loginPage()));
+                    // },
+                  ),
+
+                  InkWell(
+                    child: const Text(
+                      'See All',
+                      style: TextStyle(
+                        color: Color(0xFF4DACB2),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // onTap: (){
+                    //   Navigator.push(
+                    //     context, 
+                    //     MaterialPageRoute(
+                    //       builder: (context) => const loginPage()));
+                    // },
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border : Border.all(
+                  color: Colors.black,
+                  width: 1,
+                )
+              ),
+            ),
+
+            SizedBox(height: 5),
+
+            Container(
+              padding: EdgeInsets.all(7.0),
+              // decoration: BoxDecoration(
+              //   border: Border.all(
+              //     color: Colors.black,
+              //     width: 2,
+              //   )
+              // ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                
+                children: [
+                  InkWell(
+                    child: const Text(
+                      'News',
+                      style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // onTap: (){
+                    //   Navigator.push(
+                    //     context, 
+                    //     MaterialPageRoute(
+                    //       builder: (context) => const loginPage()));
+                    // },
+                  ),
+                ],
+              ),
+            ),
+
+
+            SizedBox(height: 5),
+
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border : Border.all(
+                  color: Colors.black,
+                  width: 1,
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  InkWell(
+                    child: const Text(
+                      'Sorry you need login',
+                      style: TextStyle(
+                        color: Color(0xFF4DACB2),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onTap: (){
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(
+                          builder: (context) => const loginPage()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+
+
           ],
         ),
       ),
